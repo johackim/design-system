b249e616c94cf8b016aab3800b9c47fa2cb38343
johackim <contact@johackim.com>
2022-04-12 22:53:03 +0200

    feat: update footer colors

--- src/footer.js
+++ src/footer.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import Link from 'next/link';
+
+import Link from '@components/link';
 
 const Footer = ({ links, date }) => (
-    <footer className="container m-auto px-4 py-8 flex items-center justify-between flex-wrap lg:max-w-screen-lg">
+    <footer className="container m-auto px-4 py-8 flex items-center justify-between flex-wrap lg:max-w-screen-lg text-gray-700 dark:text-gray-300">
         <div>
             <span>&copy;</span>
             <span className="ml-2">{date || new Date().getFullYear()}</span>
