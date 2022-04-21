83d1294495230af9d5e2763c62d29b5508c6f4a2
johackim <contact@johackim.com>
2022-04-21 16:59:55 +0200

    feat: update link component

--- src/components/link.js
+++ src/components/link.js
@@ -3,7 +3,7 @@ import NextLink from 'next/link';
 
 const Link = ({ href, children, title, className, active }) => (
     <NextLink href={href}>
-        <a href="/" className={`${active ? 'text-gray-900 dark:text-white' : 'text-gray-700 dark:text-gray-300'} hover:text-gray-900 dark:hover:text-white ${className}`} title={title}>
+        <a href={href} className={`${active ? 'text-gray-900 dark:text-white' : 'text-gray-700 dark:text-gray-300'} hover:text-gray-900 dark:hover:text-white ${className}`} title={title}>
             {children}
         </a>
     </NextLink>
