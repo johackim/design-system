cdaefacfee0075ccab0b59b2b335b1211516039f
johackim <contact@johackim.com>
2022-04-27 12:37:56 +0200

    fix: add node-resolve plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,15 @@
 import babel from '@rollup/plugin-babel';
 import postcss from 'rollup-plugin-postcss';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 export default {
     input: 'src/index.js',
     output: [
         { format: 'esm', file: 'dist/esm/index.js' },
     ],
-    external: ['react', 'react-dom', 'next/link', 'twind', 'twind/shim', '@heroicons/react/solid'],
+    external: ['react', 'react-dom', 'next/link'],
     plugins: [
+        nodeResolve(),
         babel({ babelHelpers: 'bundled' }),
         postcss(),
     ],
