59ae9e1afd56a925f2293a240bcafdff395a27b3
johackim <contact@johackim.com>
2022-04-27 16:57:27 +0200

    build: add rollup plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,21 @@
 import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
 import postcss from 'rollup-plugin-postcss';
+import { terser } from 'rollup-plugin-terser';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 export default {
     input: 'src/index.js',
     output: [
         { format: 'esm', file: 'dist/esm/index.js' },
+        { format: 'cjs', file: 'dist/cjs/index.js', exports: 'named' },
     ],
     external: ['react', 'react-dom', 'next/link'],
     plugins: [
         nodeResolve(),
         babel({ babelHelpers: 'bundled' }),
+        commonjs(),
         postcss(),
+        terser(),
     ],
 };
