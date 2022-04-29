0a7ce34d472a69a77420bafd142245d72ea6a555
johackim <contact@johackim.com>
2022-04-30 00:23:11 +0200

    feat: add button aria label

--- src/components/switch.js
+++ src/components/switch.js
@@ -13,7 +13,7 @@ const Switch = ({ defaultValue, icons, onClick }) => {
     }, []);
 
     return (
-        <button type="button" onClick={toggle} className={`${isOpen ? 'bg-gray-600' : 'bg-gray-200'} relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none`}>
+        <button type="button" aria-label="switch" onClick={toggle} className={`${isOpen ? 'bg-gray-600' : 'bg-gray-200'} relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none`}>
             {icons.length === 2 ? (
                 <span className={`${isOpen ? 'translate-x-5' : 'translate-x-0'} pointer-events-none relative inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200`}>
                     <span className={`${isOpen ? 'opacity-0 ease-out duration-100' : 'opacity-100 ease-in duration-200'} absolute inset-0 h-full w-full flex items-center justify-center transition-opacity`}>
