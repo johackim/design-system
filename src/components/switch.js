1dfa7fb250607f5424d02354933667a868b63db9
johackim <contact@johackim.com>
2022-04-30 21:43:49 +0200

    fix: update switch component

--- src/components/switch.js
+++ src/components/switch.js
@@ -10,7 +10,7 @@ const Switch = ({ defaultValue, icons, onClick }) => {
 
     useEffect(() => {
         setOpen(defaultValue);
-    }, []);
+    }, [defaultValue]);
 
     return (
         <button type="button" aria-label="switch" onClick={toggle} className={`${isOpen ? 'bg-gray-600' : 'bg-gray-200'} relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none`}>
