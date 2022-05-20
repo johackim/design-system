f0f29bedbe5566c21ea5050b0e28fa5e7ecf40a3
johackim <contact@johackim.com>
2022-05-20 17:48:45 +0200

    refactor: update empty button

--- src/components/empty.js
+++ src/components/empty.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Button from '@components/button';
+
 const Empty = ({ title, subtitle, icon, button }) => (
     <div className="text-center">
         {icon}
@@ -34,15 +36,14 @@ Empty.defaultProps = {
         </svg>
     ),
     button: (
-        <button
+        <Button
             type="button"
-            className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
         >
             <svg className="-ml-1 mr-2 h-5 w-5" x-description="Heroicon name: solid/plus" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                 <path fillRule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clipRule="evenodd" />
             </svg>
             New Project
-        </button>
+        </Button>
     ),
 };
 
