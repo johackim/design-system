8496a014291600a5c284eb9f4146c3e3f475b4a6
johackim <contact@johackim.com>
2022-10-04 18:35:00 +0200

    fix: update components args

--- src/components/input.js
+++ src/components/input.js
@@ -1,18 +1,15 @@
 import React from 'react';
 
-const Input = ({ id, type, label, name, value, onChange, className, placeholder, required, disabled }) => (
+const Input = ({ id, type, label, name, placeholder, className, disabled, ...rest }) => (
     <div className={className}>
         {label && <label htmlFor={name} className="capitalize block text-sm font-medium leading-5 text-gray-700 dark:text-gray-300">{label}</label>}
         <input
             id={id || name}
             type={type}
             name={name}
-            value={value}
-            onChange={onChange}
             className={`p-2 border border-gray-300 rounded-md focus:border-gray-300 focus:outline-none focus:shadow-outline-none focus:ring-0 text-gray-700 dark:bg-transparent dark:border-gray-500 dark:text-gray-300 dark:focus:border-gray-300 ${disabled ? 'bg-gray-200 dark:!bg-gray-700 cursor-not-allowed' : ''} ${className.includes('w-full') ? 'w-full' : ''}`}
-            placeholder={placeholder}
-            required={required}
-            disabled={disabled}
+            placeholder={placeholder || name}
+            {...rest}
         />
     </div>
 );
--- src/components/select.js
+++ src/components/select.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Select = ({ name, label, options, className }) => (
+const Select = ({ name, value, label, options, className, onChange, ...rest }) => (
     <div className={className}>
         {label && <label htmlFor={name} className="capitalize block text-sm font-medium text-gray-700 dark:text-gray-300">{label}</label>}
-        <select id={name} name={name} className={`mt-1 block pl-3 pr-10 py-2 text-base border-gray-300 focus:border-gray-300 sm:text-sm rounded-md appearance-none focus:outline-none focus:shadow-outline-none focus:ring-0 text-gray-700 dark:bg-transparent dark:text-gray-300 dark:border-gray-500 ${className.includes('w-full') ? 'w-full' : ''}`}>
+        <select id={name} name={name} onChange={onChange} value={value} className={`mt-1 block pl-3 pr-10 py-2 text-base border-gray-300 focus:border-gray-300 sm:text-sm rounded-md appearance-none focus:outline-none focus:shadow-outline-none focus:ring-0 text-gray-700 dark:bg-transparent dark:text-gray-300 dark:border-gray-500 ${className.includes('w-full') ? 'w-full' : ''}`} {...rest}>
             {options.map((option) => (
                 <option key={option} className="dark:text-gray-900">{option}</option>
             ))}
@@ -16,6 +16,7 @@ Select.defaultProps = {
     label: '',
     options: [],
     className: '',
+    onChange: () => {},
 };
 
 export default Select;
