cc0bc6ddb4474b6d67359ea12cccf2137ce320b4
johackim <contact@johackim.com>
2023-01-22 13:47:56 +0100

    feat: update message

--- src/components/message.js
+++ src/components/message.js
@@ -1,24 +1,48 @@
 import React from 'react';
-import { ExclamationCircleIcon, InformationCircleIcon } from '@heroicons/react/outline';
+import { ExclamationIcon, CheckCircleIcon, InformationCircleIcon, XCircleIcon } from '@heroicons/react/solid';
 
-const Message = ({ text, warning, info }) => (
-    <div className="bg-gray-100 dark:bg-gray-800 border-l-4 border-gray-400 p-4">
-        <div className="flex">
-            {warning && <div><ExclamationCircleIcon className="w-5 h-5 text-gray-400" /></div>}
-            {info && <div><InformationCircleIcon className="w-5 h-5 text-gray-400" /></div>}
-            <div className="ml-3">
-                <p className="text-sm text-gray-400" style={{ marginTop: 0, marginBottom: 0 }}>
-                    {text}
-                </p>
+const Message = ({ children, type }) => {
+    const textClassName = {
+        error: 'text-red-700',
+        info: 'text-gray-700',
+        warning: 'text-yellow-700',
+        success: 'text-green-700',
+    };
+
+    const iconClassName = {
+        error: 'text-red-400',
+        info: 'text-gray-400',
+        warning: 'text-yellow-400',
+        success: 'text-green-400',
+    };
+
+    const bgClassName = {
+        error: 'bg-red-50 border-red-400 dark:bg-red-800',
+        info: 'bg-gray-50 border-gray-400 dark:bg-gray-800',
+        warning: 'bg-yellow-50 border-yellow-400 dark:bg-yellow-800',
+        success: 'bg-green-50 border-green-400 dark:bg-green-800',
+    };
+
+    return (
+        <div className={`${bgClassName[type]} border-l-4 p-4`}>
+            <div className="flex">
+                {type === 'info' && <div><InformationCircleIcon className={`w-5 h-5 ${iconClassName[type]}`} /></div>}
+                {type === 'error' && <div><XCircleIcon className={`w-5 h-5 ${iconClassName[type]}`} /></div>}
+                {type === 'warning' && <div><ExclamationIcon className={`w-5 h-5 ${iconClassName[type]}`} /></div>}
+                {type === 'success' && <div><CheckCircleIcon className={`w-5 h-5 ${iconClassName[type]}`} /></div>}
+                <div className="ml-3">
+                    <p className={`text-sm ${textClassName[type]}`} style={{ marginTop: 0, marginBottom: 0 }}>
+                        {children}
+                    </p>
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
 
 Message.defaultProps = {
-    text: 'Hello world',
-    warning: false,
-    info: false,
+    type: 'info',
+    children: 'hello world',
 };
 
 export default Message;
--- src/components/message.stories.js
+++ src/components/message.stories.js
@@ -10,3 +10,24 @@ const Template = (args) => <Message {...args} />;
 export const Default = Template.bind({});
 
 Default.args = Message.defaultProps;
+
+export const Warning = Template.bind({});
+
+Warning.args = {
+    ...Default.args,
+    type: 'warning',
+};
+
+export const Error = Template.bind({});
+
+Error.args = {
+    ...Default.args,
+    type: 'error',
+};
+
+export const Success = Template.bind({});
+
+Success.args = {
+    ...Default.args,
+    type: 'success',
+};
