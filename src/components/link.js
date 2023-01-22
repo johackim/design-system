b7cbddf91821ce06cb86e25c94adf52d2ed54a66
johackim <contact@johackim.com>
2023-01-22 13:47:23 +0100

    feat: update link

--- src/components/link.js
+++ src/components/link.js
@@ -2,10 +2,8 @@ import React from 'react';
 import NextLink from 'next/link';
 
 const Link = ({ href, children, className, active, ...rest }) => (
-    <NextLink href={href}>
-        <a href={href} className={`${active ? 'text-gray-900 dark:text-white' : 'text-gray-700 dark:text-gray-300'} hover:text-gray-900 dark:hover:text-white ${className}`} {...rest}>
-            {children}
-        </a>
+    <NextLink href={href} className={`${active ? 'text-gray-900 dark:text-white' : 'text-gray-700 dark:text-gray-300'} hover:text-gray-900 dark:hover:text-white ${className}`.trim()} {...rest}>
+        {children}
     </NextLink>
 );
 
