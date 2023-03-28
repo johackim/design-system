5416fb4c594e3a7cfcc86801f3a4ae1bf2f87cd4
johackim <contact@johackim.com>
2023-03-28 14:01:21 +0200

    feat: update card component

--- src/molecules/card.js
+++ src/molecules/card.js
@@ -1,25 +1,23 @@
 import React from 'react';
 
-const Card = ({ small, name, value, right, center, bottom, ...rest }) => (
+const Card = ({ small, name, value, header, children, ...rest }) => (
     small ? (
         <div key={name} className="px-4 py-5 bg-white dark:bg-gray-800 shadow rounded-lg overflow-hidden sm:p-4 flex flex-col" {...rest}>
             <div>
-                <span className="text-sm text-gray-700 dark:text-gray-300 float-right">{right}</span>
+                {header}
                 <dt className="text-sm font-semibold dark:text-white">{name}</dt>
                 {value && <dd className="text-sm text-gray-500">{value}</dd>}
             </div>
-            {center}
-            {bottom}
+            {children}
         </div>
     ) : (
         <div key={name} className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-4 dark:bg-gray-800 flex flex-col" {...rest}>
             <div>
-                <span className="text-sm float-right">{right}</span>
+                {header}
                 <dt className="truncate text-sm font-medium text-gray-500 dark:text-white">{name}</dt>
                 {value && <dd className="mt-1 text-3xl font-semibold text-gray-900 dark:text-white">{value}</dd>}
             </div>
-            {center}
-            {bottom}
+            {children}
         </div>
     )
 );
@@ -28,8 +26,7 @@ Card.defaultProps = {
     name: 'name',
     value: 'value',
     small: false,
-    right: '',
-    bottom: '',
+    header: null,
 };
 
 export default Card;
