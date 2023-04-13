c140d0e3325501558134b5a603c4d27d6d29c7a7
johackim <contact@johackim.com>
2023-04-13 13:47:10 +0200

    feat: update card

--- src/molecules/card.js
+++ src/molecules/card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({ small, name, value, header, children, ...rest }) => (
     small ? (
-        <div key={name} className="px-4 py-5 bg-white dark:bg-gray-800 shadow rounded-lg overflow-hidden sm:p-4 flex flex-col" {...rest}>
+        <div key={name} className="px-4 py-5 bg-white dark:bg-gray-800 shadow rounded-lg overflow-hidden sm:p-4 flex flex-col relative" {...rest}>
             <div>
                 {header}
                 <dt className="text-sm font-semibold dark:text-white">{name}</dt>
@@ -11,7 +11,7 @@ const Card = ({ small, name, value, header, children, ...rest }) => (
             {children}
         </div>
     ) : (
-        <div key={name} className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-4 dark:bg-gray-800 flex flex-col" {...rest}>
+        <div key={name} className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-4 dark:bg-gray-800 flex flex-col relative" {...rest}>
             <div>
                 {header}
                 <dt className="truncate text-sm font-medium text-gray-500 dark:text-white">{name}</dt>
