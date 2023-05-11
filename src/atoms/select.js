14d76f788e1e4bffcfbcacb941475d5ae389e8ec
johackim <contact@johackim.com>
2023-05-11 18:21:42 +0200

    fix: swap option name and value

--- src/atoms/select.js
+++ src/atoms/select.js
@@ -6,7 +6,7 @@ const Select = ({ name, value, label, options, className, onChange, ...rest }) =
         <select id={name} name={name} onChange={onChange} value={value} className={`mt-1 block pl-3 pr-10 py-2 text-base border-gray-300 focus:border-gray-300 sm:text-sm rounded-md appearance-none focus:outline-none focus:shadow-outline-none focus:ring-0 text-gray-700 dark:bg-transparent dark:text-gray-300 dark:border-gray-500 ${className.includes('w-full') ? 'w-full' : ''}`} {...rest}>
             {options.map((option) => (
                 typeof option === 'object' ? (
-                    <option key={option.name} value={option.name}>{option.value}</option>
+                    <option key={option.name} value={option.value}>{option.name}</option>
                 ) : (
                     <option key={option} value={option}>{option}</option>
                 )
