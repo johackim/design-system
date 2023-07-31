dbb51818fff2ff813097635d8c423e80cfbc0d86
johackim <contact@johackim.com>
2023-07-31 11:52:24 +0200

    chore: update storybook settings

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,14 +6,6 @@ export default {
         '@storybook/addon-links',
         '@storybook/addon-essentials',
         '@storybook/addon-interactions',
-        {
-            name: '@storybook/addon-postcss',
-            options: {
-                postcssLoaderOptions: {
-                    implementation: require('postcss'),
-                },
-            },
-        },
     ],
     framework: {
         name: '@storybook/react-webpack5',
