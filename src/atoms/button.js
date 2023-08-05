b5762efff22d67b3aa0837e9702a976d954fb7d6
johackim <contact@johackim.com>
2023-08-05 22:21:58 +0200

    fix: update button colors

--- src/atoms/button.js
+++ src/atoms/button.js
@@ -4,7 +4,7 @@ import Loading from '@atoms/loading';
 const Button = ({ secondary, children, onClick, className, disabled, loading, ...rest }) => {
     const baseClassName = `inline-flex rounded-md items-center justify-center px-5 py-2 border focus:outline-none disabled:cursor-not-allowed disabled:opacity-75 ${className}`;
 
-    const primaryClassName = `${baseClassName} border-transparent text-white !bg-gray-600 hover:!bg-gray-700 disabled:hover:!bg-gray-600 disabled:dark:hover:bg-gray-700 dark:bg-gray-700 dark:hover:bg-gray-600 dark:hover:text-white disabled:dark:hover:!text-gray-300 dark:text-gray-300`;
+    const primaryClassName = `${baseClassName} border-transparent text-white !bg-gray-600 hover:!bg-gray-700 disabled:hover:!bg-gray-600 disabled:dark:hover:bg-gray-700 dark:!bg-gray-700 dark:hover:!bg-gray-600 dark:hover:text-white disabled:dark:hover:!text-gray-300 dark:text-gray-300`;
 
     const secondaryClassName = `${baseClassName} border-gray-300 text-gray-700 hover:text-gray-900 disabled:hover:text-gray-700 disabled:hover:border-gray-300 hover:border-gray-400 dark:text-gray-300 dark:hover:text-white dark:hover:border-white`;
 
