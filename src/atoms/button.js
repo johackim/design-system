b592c1617ee151bc953011f0e577a850e5defb2c
johackim <contact@johackim.com>
2023-08-08 22:25:38 +0200

    feat: update button

--- src/atoms/button.js
+++ src/atoms/button.js
@@ -1,21 +1,29 @@
 import React from 'react';
+import { cva } from 'class-variance-authority';
 import Loading from '@atoms/loading';
 
-const Button = ({ secondary, children, onClick, className, disabled, loading, ...rest }) => {
-    const baseClassName = `inline-flex rounded-md items-center justify-center px-5 py-2 border focus:outline-none disabled:cursor-not-allowed disabled:opacity-75 ${className}`;
+const style = cva(
+    'inline-flex items-center px-5 py-2 rounded-md disabled:cursor-not-allowed disabled:opacity-75',
+    {
+        variants: {
+            secondary: {
+                false: '!bg-gray-600 text-white hover:text-white hover:!bg-gray-700 disabled:hover:bg-gray-600 disabled:dark:hover:!bg-gray-700 dark:!bg-gray-700 dark:hover:!bg-gray-600',
+                true: 'border border-gray-300 hover:border-gray-400 text-gray-700 hover:text-gray-900 disabled:hover:border-gray-300 dark:text-gray-300 dark:hover:text-white dark:hover:border-white',
+            },
+        },
+        defaultVariants: {
+            secondary: false,
+        },
+    },
+);
 
-    const primaryClassName = `${baseClassName} border-transparent text-white !bg-gray-600 hover:!bg-gray-700 disabled:hover:!bg-gray-600 disabled:dark:hover:bg-gray-700 dark:!bg-gray-700 dark:hover:!bg-gray-600 dark:hover:text-white disabled:dark:hover:!text-gray-300 dark:text-gray-300`;
+const Button = ({ secondary, children, onClick, className, disabled, loading, ...rest }) => (
+    <button type="button" onClick={onClick} className={style({ className, secondary })} disabled={disabled || loading} {...rest}>
+        {loading ? <Loading text="Loading..." className={secondary ? '' : 'text-white'} /> : children}
+    </button>
+);
 
-    const secondaryClassName = `${baseClassName} border-gray-300 text-gray-700 hover:text-gray-900 disabled:hover:text-gray-700 disabled:hover:border-gray-300 hover:border-gray-400 dark:text-gray-300 dark:hover:text-white dark:hover:border-white`;
-
-    const buttonClassName = secondary ? secondaryClassName : primaryClassName;
-
-    return (
-        <button type="button" onClick={onClick} className={buttonClassName} disabled={disabled || loading} {...rest}>
-            {loading ? <Loading text="Loading..." className={secondary ? '' : 'text-white'} /> : children}
-        </button>
-    );
-};
+Button.style = style;
 
 Button.defaultProps = {
     onClick: () => {},
--- src/atoms/link.js
+++ src/atoms/link.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import NextLink from 'next/link';
+import { cva } from 'class-variance-authority';
 
-const Link = ({ href, children, className, active, ...rest }) => (
-    <NextLink href={href} className={`${active ? 'text-gray-900 dark:text-white' : 'text-gray-700 dark:text-gray-300'} hover:text-gray-900 dark:hover:text-white ${className}`.trim()} {...rest}>
+const style = cva(
+    'text-gray-700 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white',
+    {
+        variants: {
+            active: {
+                true: 'text-gray-900 dark:text-white',
+            },
+        },
+        defaultVariants: {
+            active: false,
+        },
+    },
+);
+
+const Link = ({ href, children, active, as, className, secondary = false, ...props }) => (
+    <NextLink href={href} className={style({ className: `${as ? as.style({ secondary }) : ''} ${className}`.trim(), active })} {...props}>
         {children}
     </NextLink>
 );
+
+Link.style = style;
 
 Link.defaultProps = {
     href: '/',
