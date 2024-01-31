b5a8a75e52bf8ed5b2ee6192afc92dc96dea9d36
johackim <contact@johackim.com>
2024-01-31 02:23:42 +0100

    refactor: update tooltip

--- src/atoms/tooltip.js
+++ src/atoms/tooltip.js
@@ -1,30 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-const Tooltip = ({ children, text }) => {
-    const [showTip, setShowTip] = useState(false);
-
-    const handleMouseOver = () => setShowTip(true);
-
-    const handleMouseOut = () => setShowTip(false);
-
-    return (
-        <div className="relative inline-flex">
-            <div
-                onMouseOver={handleMouseOver}
-                onFocus={handleMouseOver}
-                onMouseOut={handleMouseOut}
-                onBlur={handleMouseOut}
-            >
-                {children}
-            </div>
-            {showTip && (
-                <div className="absolute w-max max-w-[300px] text-center z-50 rounded-md p-2 text-sm bg-black text-white bottom-[110%] left-1/2 transform -translate-x-1/2">
-                    {text}
-                </div>
-            )}
+const Tooltip = ({ children, text }) => (
+    <div className="relative inline-flex align-bottom group">
+        <div>
+            {children}
         </div>
-    );
-};
+        <div className="hidden group-hover:block absolute w-max max-w-[300px] text-center z-50 rounded-md p-2 text-sm bg-black text-white bottom-[110%] left-1/2 transform -translate-x-1/2">
+            {text}
+        </div>
+    </div>
+);
 
 Tooltip.defaultProps = {
     text: 'Tooltip',
